ec40f608890e12ea44cd7d24cccaf1437b87ded5
sindydwns <sindydwns@naver.com>
2023-03-19 05:01:22 +0900

    add debug mode

--- src/auth/auth-guard.ts
+++ src/auth/auth-guard.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
+import { AppDataSource } from '../database';
+import User from '../entity/user.entity';
 
 @Injectable()
 export class AuthGuard extends PassportAuthGuard('session') {
-  canActivate(context) {
+  async canActivate(context) {
+    // TODO: await 로 바꾸기
     const request = context.switchToHttp().getRequest();
-    return super.canActivate(context) && request.session.user != null;
+    if (request.session.user == null && process.env.DEBUG_INTRA_ID)
+      request.session.user = await AppDataSource.getRepository(User).findOne({
+        where: { intraId: process.env.DEBUG_INTRA_ID },
+      });
+    return super.canActivate(context) && request.session.user?.intraId != null;
   }
 }
